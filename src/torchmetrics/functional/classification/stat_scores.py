174971083722fcdbeb77751d9bb9a5f140431319
yellowdolphin <marius.wanko@gmail.com>
2022-08-23 14:02:18 +0200

    debug top_k acc

--- src/torchmetrics/functional/classification/stat_scores.py
+++ src/torchmetrics/functional/classification/stat_scores.py
@@ -160,8 +160,8 @@ def _stat_scores_update(
         top_k=top_k,
         ignore_index=ignore_index,
     )
-    print("preds:", preds)  # DEBUG
-    print("target:", target)  # DEBUG
+    print("preds:", preds.sum(dim=1))  # DEBUG
+    print("target:", target.sum(dim=1))  # DEBUG
 
     if ignore_index is not None and ignore_index >= preds.shape[1]:
         raise ValueError(f"The `ignore_index` {ignore_index} is not valid for inputs with {preds.shape[1]} classes")
